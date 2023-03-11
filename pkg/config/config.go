fe251df7da3b77b4af7f13324fc93995cc21aaff
mpapenbr <Markus.Papenbrock+github@gmail.com>
2023-03-11 12:22:34 +0100

    added connectity checks (#11)

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -2,12 +2,13 @@ package config
 
 // this holds the resolved configuration values
 var (
-	DB          string // connection string for the database
-	URL         string // URL of WAMP server
-	Realm       string // realm to use
-	Password    string // password for backend
-	LogLevel    string // sets the log level (zap log level values)
-	SQLLogLevel string // sets the log level for sql subsystem
-	LogFormat   string // text vs json
+	DB              string // connection string for the database
+	URL             string // URL of WAMP server
+	Realm           string // realm to use
+	Password        string // password for backend
+	WaitForServices string // duration to wait for other services to be ready
+	LogLevel        string // sets the log level (zap log level values)
+	SQLLogLevel     string // sets the log level for sql subsystem
+	LogFormat       string // text vs json
 
 )
