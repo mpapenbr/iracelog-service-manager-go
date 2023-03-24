39ed81595ad299e28205a53dec06806010b1b9dc
mpapenbr <Markus.Papenbrock+github@gmail.com>
2023-03-24 15:57:24 +0100

    Data delivery broken Fixes #12

--- pkg/repository/state/state_repos_test.go
+++ pkg/repository/state/state_repos_test.go
@@ -1,10 +1,26 @@
-//nolint:funlen,lll // ok for param tests
+//nolint:funlen,lll,errcheck // ok for param tests
 package state
 
 import (
+	"context"
+	"log"
 	"reflect"
 	"testing"
+
+	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/stretchr/testify/assert"
+
+	"github.com/mpapenbr/iracelog-service-manager-go/pkg/model"
+	"github.com/mpapenbr/iracelog-service-manager-go/pkg/repository/event"
+	tcpg "github.com/mpapenbr/iracelog-service-manager-go/testsupport/tcpostgres"
 )
+
+func initTestDb() *pgxpool.Pool {
+	pool := tcpg.SetupTestDb()
+	tcpg.ClearAllTables(pool)
+	return pool
+}
 
 func Test_computeCarChanges(t *testing.T) {
 	type args struct {
@@ -123,6 +139,75 @@ func Test_computeSessionChanges(t *testing.T) {
 			if got := computeSessionChanges(tt.args.ref, tt.args.cur); !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("computeSessionChanges() = %v, want %v", got, tt.want)
 			}
+		})
+	}
+}
+
+func TestLoadByEventIdWithDelta(t *testing.T) {
+	db := initTestDb()
+	dbEvent := &model.DbEvent{
+		Name:        "Test",
+		Key:         "testKey",
+		Description: "myDescr",
+		Data:        model.EventData{},
+	}
+
+	err := pgx.BeginFunc(context.Background(), db, func(tx pgx.Tx) error {
+		e, err := event.Create(tx.Conn(), dbEvent)
+		for i := 1; i <= 3; i++ {
+			Create(tx.Conn(), &model.DbState{
+				EventID: e.ID,
+				Data: model.StateData{
+					Type: 1,
+					Payload: model.StatePayload{
+						Cars: [][]interface{}{
+							{i, 2},
+						},
+						Session: []any{i, 2},
+					}, Timestamp: float64(i * 10.0),
+				},
+			})
+		}
+		return err
+	})
+	if err != nil {
+		log.Fatalf("createTestdata: %v\n", err)
+	}
+
+	var type1 int = 1
+	type args struct {
+		startTS float64
+		num     int
+	}
+	tests := []struct {
+		name      string
+		args      args
+		wantRef   any
+		wantDelta []int
+		wantErr   bool
+	}{
+		{name: "all", args: args{startTS: 0, num: 10}, wantRef: &type1, wantDelta: []int{2, 2}},
+		{name: "none", args: args{startTS: 0, num: 0}, wantRef: nil, wantDelta: []int{}},
+		{name: "oob", args: args{startTS: 30, num: 10}, wantRef: nil, wantDelta: []int{}},
+		{name: "last", args: args{startTS: 20, num: 10}, wantRef: &type1, wantDelta: []int{}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, got1, err := LoadByEventIdWithDelta(db, dbEvent.ID, tt.args.startTS, tt.args.num)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("LoadByEventIdWithDelta() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != nil {
+				assert.Equal(t, tt.wantRef, &got.Type)
+			} else {
+				assert.Equal(t, tt.wantRef, nil)
+			}
+			rest := make([]int, 0)
+			for _, v := range got1 {
+				rest = append(rest, v.Type)
+			}
+			assert.Equal(t, tt.wantDelta, rest)
 		})
 	}
 }
