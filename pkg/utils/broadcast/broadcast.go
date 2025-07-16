220ac2353094b9a9bbab1a92a390a2d02037fe2f
mpapenbr <Markus.Papenbrock+github@gmail.com>
2025-07-16 11:03:24 +0200

    Bug: Metrics for broadcaster incomplete Fixes #299

--- pkg/utils/broadcast/broadcast.go
+++ pkg/utils/broadcast/broadcast.go
@@ -34,7 +34,12 @@ type broadcastServer[T any] struct {
 	numSnd         int
 	numSkip        int
 	eventKey       string
+	mData          *mData
+	l              *log.Logger
 }
+
+// check interface compliance
+var _ BroadcastServer[any] = (*broadcastServer[any])(nil)
 
 type Option[T any] func(*broadcastServer[T])
 
@@ -55,7 +60,7 @@ func (b *broadcastServer[T]) CancelSubscription(ch <-chan T) {
 }
 
 func (b *broadcastServer[T]) Close() {
-	log.Info("Closing broadcast server",
+	b.l.Info("Closing broadcast server",
 		log.String("name", b.name),
 		log.Int("rcv", b.numRcv), log.Int("snd", b.numSnd), log.Int("skip", b.numSkip))
 	b.cancel()
@@ -75,72 +80,146 @@ func NewBroadcastServer[T any](
 		removeListener: make(chan (<-chan T)),
 		ctx:            ctx,
 		cancel:         cancel,
+		l:              log.Default().Named(fmt.Sprintf("bcst.%s", name)),
 	}
 	b.setupMetrics()
 	go b.serve()
 	return b
 }
 
+type (
+	bCstMetricInfo struct {
+		metricName string
+		gauge      metric.Int64ObservableGauge
+		reg        metric.Registration
+	}
+	mData struct {
+		meter                    metric.Meter
+		rcv, snd, skip, listener *bCstMetricInfo
+	}
+)
+
+var myMeter = otel.GetMeterProvider().Meter("ism.broadcast")
+
+func (b *broadcastServer[T]) createMData(meter metric.Meter) (*mData, error) {
+	ret := &mData{meter: meter}
+
+	type data struct {
+		name, desc, unit string
+		assign           func(metricName string, gauge metric.Int64ObservableGauge)
+	}
+	for _, d := range []*data{
+		{
+			"ism.broadcast.rcv", "Number of received messages", "{count}",
+			func(metricName string, gauge metric.Int64ObservableGauge) {
+				ret.rcv = &bCstMetricInfo{metricName: metricName, gauge: gauge}
+			},
+		},
+		{
+			"ism.broadcast.snd", "Number of sent messages", "{count}",
+			func(metricName string, gauge metric.Int64ObservableGauge) {
+				ret.snd = &bCstMetricInfo{metricName: metricName, gauge: gauge}
+			},
+		},
+		{
+			"ism.broadcast.skip", "Number of skipped messages", "{count}",
+			func(metricName string, gauge metric.Int64ObservableGauge) {
+				ret.skip = &bCstMetricInfo{metricName: metricName, gauge: gauge}
+			},
+		},
+		{
+			"ism.broadcast.listener", "Number of listeners", "{count}",
+			func(metricName string, gauge metric.Int64ObservableGauge) {
+				ret.listener = &bCstMetricInfo{metricName: metricName, gauge: gauge}
+			},
+		},
+	} {
+		if gauge, err := meter.Int64ObservableGauge(
+			d.name,
+			metric.WithDescription(d.desc),
+			metric.WithUnit(d.unit),
+		); err != nil {
+			return nil, err
+		} else {
+			d.assign(d.name, gauge)
+		}
+	}
+	return ret, nil
+}
+
 //nolint:lll,funlen // readability
 func (b *broadcastServer[T]) setupMetrics() {
-	// Implement the setupMetrics method here
-	// For example, initialize metrics or logging
-	log.Info("Setting up metrics",
+	b.l.Info("Setting up metrics",
 		log.String("eventKey", b.eventKey),
 		log.String("name", b.name))
-	meter := otel.GetMeterProvider().Meter(fmt.Sprintf("ism.broadcast.%s", b.name))
-	register := func(metricName, desc, unit string, valueProvider func() int64) {
-		if _, err := meter.Int64ObservableGauge(
-			metricName,
-			metric.WithDescription(desc),
-			metric.WithUnit(unit),
+	var err error
+	var mData *mData
+	if mData, err = b.createMData(myMeter); err != nil {
+		b.l.Error("failed to create metric",
+			log.String("eventKey", b.eventKey),
+			log.String("name", b.name),
+			log.ErrorField(err))
+		return
+	} else {
+		b.mData = mData
+	}
 
-			metric.WithInt64Callback(func(_ context.Context, o metric.Int64Observer) error {
-				o.Observe(valueProvider(),
+	type data struct {
+		info  *bCstMetricInfo
+		value func() int64
+	}
+	for _, d := range []*data{
+		{
+			mData.rcv,
+			func() int64 { return int64(b.numRcv) },
+		},
+		{
+			mData.listener,
+			func() int64 { return int64(len(b.listeners)) },
+		},
+		{
+			mData.snd,
+			func() int64 { return int64(b.numSnd) },
+		},
+		{
+			mData.skip,
+			func() int64 { return int64(b.numSkip) },
+		},
+	} {
+		var err error
+		d.info.reg, err = myMeter.RegisterCallback(
+			func(_ context.Context, o metric.Observer) error {
+				b.l.Debug("calling metric callback",
+					log.String("metric", d.info.metricName),
+					log.String("name", b.name),
+					log.String("event", b.eventKey))
+				o.ObserveInt64(d.info.gauge, d.value(),
 					metric.WithAttributes(
 						attribute.String("name", b.name),
 						attribute.String("event", b.eventKey),
 					),
 				)
 				return nil
-			})); err != nil {
-			log.Error("failed to register metric",
-				log.String("metric", metricName),
+			},
+			d.info.gauge,
+		)
+		if err != nil {
+			b.l.Error("failed to register metric callback",
+				log.String("metric", d.info.metricName),
 				log.ErrorField(err))
+		} else {
+			b.l.Debug("registered metric callback",
+				log.String("metric", d.info.metricName),
+				log.String("name", b.name),
+				log.String("event", b.eventKey))
 		}
-	}
-	type data struct {
-		name  string
-		desc  string
-		unit  string
-		value func() int64
-	}
-	for _, d := range []*data{
-		{
-			"ism.broadcast.rcv", "Number of received messages", "{count}",
-			func() int64 { return int64(b.numRcv) },
-		},
-		{
-			"ism.broadcast.snd", "Number of sent messages", "{count}",
-			func() int64 { return int64(b.numSnd) },
-		},
-		{
-			"ism.broadcast.skip", "Number of skipped messages", "{count}",
-			func() int64 { return int64(b.numSkip) },
-		},
-		{
-			"ism.broadcast.listener", "Number of listeners", "{count}",
-			func() int64 { return int64(len(b.listeners)) },
-		},
-	} {
-		register(d.name, d.desc, d.unit, d.value)
 	}
 }
 
 //nolint:funlen,cyclop,gocognit // by design
 func (b *broadcastServer[T]) serve() {
 	defer func() {
-		log.Info("Closing listeners", log.String("name", b.name))
+		b.l.Info("Closing listeners", log.String("name", b.name))
 		for _, listener := range b.listeners {
 			if listener != nil {
 				close(listener)
@@ -151,29 +230,30 @@ func (b *broadcastServer[T]) serve() {
 	for {
 		select {
 		case <-b.ctx.Done():
-			log.Info("broadcast server about to be closed", log.String("name", b.name))
+			b.l.Info("broadcast server about to be closed", log.String("name", b.name))
+			b.unregisterMetrics()
 			return
 		case ch := <-b.addListener:
 			b.listeners = append(b.listeners, ch)
 		case ch := <-b.removeListener:
-			log.Debug("removing listener",
+			b.l.Debug("removing listener",
 				log.String("name", b.name), log.Int("len", len(b.listeners)))
 			m.Lock()
-			log.Debug("got lock")
+			b.l.Debug("got lock")
 			for i, listener := range b.listeners {
 				if listener == ch {
 					b.listeners = append(b.listeners[:i], b.listeners[i+1:]...)
 
-					log.Debug("removed listener",
+					b.l.Debug("removed listener",
 						log.String("name", b.name), log.Int("len", len(b.listeners)))
 
 					close(listener)
-					log.Debug("closed channel",
+					b.l.Debug("closed channel",
 						log.String("name", b.name))
 					// break
 				}
 			}
-			log.Debug("unlocking",
+			b.l.Debug("unlocking",
 				log.String("name", b.name), log.Int("len", len(b.listeners)))
 			m.Unlock()
 		case msg := <-b.source:
@@ -195,6 +275,30 @@ func (b *broadcastServer[T]) serve() {
 			}
 
 			m.Unlock()
+		}
+	}
+}
+
+func (b *broadcastServer[T]) unregisterMetrics() {
+	for _, info := range []*bCstMetricInfo{
+		b.mData.rcv, b.mData.snd, b.mData.skip, b.mData.listener,
+	} {
+		if info.reg != nil {
+			if err := info.reg.Unregister(); err != nil {
+				b.l.Error("failed to unregister metric",
+					log.String("metric", info.metricName),
+					log.ErrorField(err))
+			} else {
+				b.l.Debug("unregistered metric",
+					log.String("metric", info.metricName),
+					log.String("name", b.name),
+					log.String("event", b.eventKey))
+			}
+		} else {
+			b.l.Debug("no registration to unregister",
+				log.String("metric", info.metricName),
+				log.String("name", b.name),
+				log.String("event", b.eventKey))
 		}
 	}
 }
